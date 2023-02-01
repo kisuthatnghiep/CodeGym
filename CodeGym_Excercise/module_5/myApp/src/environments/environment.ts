0d5189282a3ec003a007e4fa07248b67c06d79da
kisuthatnghiep <ntthanhdb1999@gmail.com>
2023-02-01 20:35:45 +0700

    add file

--- CodeGym_Excercise/module_5/myApp/src/environments/environment.ts
+++ CodeGym_Excercise/module_5/myApp/src/environments/environment.ts
@@ -1,4 +1,12 @@
 export const environment = {
   production: false,
+  firebaseConfig: {
+    apiKey : "AIzaSyChxmHySQmYuVDi6zvSXjNqJEuRGQRf8kg" ,
+    authDomain : "test-18418.firebaseapp.com" ,
+    projectId : "test-18418" ,
+    storageBucket : "test-18418.appspot.com" ,
+    messagingSenderId : "943622110163" ,
+    appId : "1:943622110163:web:9f60d8121eaeab2b07cd26"
+  },
   apiUrl: 'http://localhost:8080/'
 };
